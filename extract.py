997db37ff13107659d4370ae6e4cd748d0f6913c
arjunfzk <32428467+arjunfzk@users.noreply.github.com>
2023-06-27 01:20:14 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -12,7 +12,7 @@ def createDriver() -> webdriver.Chrome:
     # chrome_options.headless = True
 
 
-    chrome_options.add_experimental_option("prefs", prefs)
+    #chrome_options.add_experimental_option("prefs", prefs)
     myDriver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
     return myDriver
